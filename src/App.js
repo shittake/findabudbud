edd107a1c497dc6398e3d0d9a230360317e9cf5c
ericleow <40803540+shittake@users.noreply.github.com>
2022-05-21 13:16:09 +1000

    Add caption to homepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h3> I think this may be easier </h3>
+      <h4> why is this different? </h4>
     </div>
   );
 }
