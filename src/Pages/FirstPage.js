d83c97628083418c6c209a085eef963f7335a31e
shittake <ericleowyuquan@gmail.com>
2022-05-27 01:00:49 +1000

    Add code fragment to fetch data from supabase database

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -5,13 +5,28 @@ import { supabase } from "../supabaseClient";
 const FirstPage = ({session}) => {
 	const [username, setUsername] = useState(null);
   	const [brawl_stars, setBrawlStars] = useState(null);
+
+  	const [users, setUsers] = useState([]);
+
+  	const fetchData = async () => {
+  		const {data, error} = await supabase
+  		.from('profiles')
+  		.select('username')
+
+  		console.log(data);
+
+  		setUsers(data);
+  	}
+
+  	useEffect(() => {
+  		fetchData();
+  	},[])
+
+
   
 return(
 	<>
-		<div>
-		This is the first page.
-		Testing
-		</div>
+	{users.map(user => <p>{user.username}</p>)}
 	</>);	
 }
 
