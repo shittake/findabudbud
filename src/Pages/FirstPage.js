209fb1b8fbec5dcd8921b1b8bb1ad854270dd4dc
shittake <ericleowyuquan@gmail.com>
2022-05-26 18:44:37 +1000

    import useEffect method from react to homepage

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from 'react';
 import { supabase } from "../supabaseClient";
 
 const FirstPage = ({session}) => {
 	const [username, setUsername] = useState(null);
   	const [brawl_stars, setBrawlStars] = useState(null);
-
+  
 return(
 	<>
 		<div>
