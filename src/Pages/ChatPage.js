ee0b9821166aec17772832005dcaccbcffc7096b
shittake <ericleowyuquan@gmail.com>
2022-05-28 23:48:44 +1000

    add preliminary files for instant chat function

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -1,14 +1,54 @@
-import { useState, useEffect } from "react";
-import React from 'react';
-import { supabase } from "../supabaseClient";
+require('./ChatPage.css');
+require('./Login.css');
 
-const ChatPage = () => {
-	return (
-		<>
-			<div>
-				This is for chatting.
-			</div>
-		</>);
+import React from 'react';
+import ChatApp from './ChatApp';
+
+class ChatPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+
+    // Bind 'this' to event handlers. React ES6 does not do this by default
+    this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
+    this.usernameSubmitHandler = this.usernameSubmitHandler.bind(this);
+  }
+
+  usernameChangeHandler(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  usernameSubmitHandler(event) {
+    event.preventDefault();
+    this.setState({ submitted: true, username: this.state.username });
+  }
+
+  render() {
+    if (this.state.submitted) {
+      // Form was submitted, now show the main App
+      return (
+        <ChatApp username={this.state.username} />
+      );
+    }
+
+    // Initial page load, show a simple login form
+    return (
+      <form onSubmit={this.usernameSubmitHandler} className="username-container">
+        <h1>React Instant Chat</h1>
+        <div>
+          <input
+            type="text"
+            onChange={this.usernameChangeHandler}
+            placeholder="Enter a username..."
+            required />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+
 }
+ChatPage.defaultProps = {
+};
 
 export default ChatPage;
