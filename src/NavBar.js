efabdef57c9ce4a0e04d201f13bbfc50245249a6
shittake <ericleowyuquan@gmail.com>
2022-06-07 01:17:54 +1000

    add image to replace links used by NavBar and Router

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import videologo from "./Images/VideoOfTheDay.png"
+
 
 function NavBar() {
 	return (	
 		<nav>
 			<ul>
-				<li>
-					<Link to="/">Video of the Day</Link>
-				</li>
+				<Link to="/">
+					<h1 className="homepageimage">
+          				<img src={videologo} class="centerimage" height="170" style ={{cursor:'pointer'}} />
+        			</h1>
+				</Link>
+
 
 				<li>
 					<Link to="firstpage">People who love brawl stars</Link>
