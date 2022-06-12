6eec6534f3b2c0c1263e29b8bcdcc13ea8999c55
shittake <ericleowyuquan@gmail.com>
2022-06-12 22:10:02 +1000

    preliminary randomized video function that currently does not work

--- src/Pages/VideoPage.js
+++ src/Pages/VideoPage.js
@@ -1,13 +1,65 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "../supabaseClient";
 import Header from "../Header";
 import Footer from "../Footer";
 import ChatwootWidget from "../chatwoot.js";
 
 const VideoPage = ({ session }) => {
-    return (
-      <div>
-        <Header session={session} />
 
+    const [date, setDate] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [link, setLink] = useState(null);
+
+    var links = ["https://www.youtube.com/embed/G_wxQe2Litw","https://www.youtube.com/embed/FT5OYVyc7Sw"];
+    var titles = ["Brawl Stars Tips", "Clash Royale Tips"];
+
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("profiles").select("*");
+
+      setDate(data.date);
+      setIndex(data.index);
+      setLink(links[index]);
+
+      var currMinute = new Date().getMinutes();
+
+      if (currMinute != date) {
+        updateIndex(currMinute);
+        setDate(currMinute);
+        setLink(links[1]);
+      }
+    };
+
+    useEffect(() => {
+      fetchData();
+      }, [session]);
+
+    const updateIndex = async (number) => {
+      setLoading(true);
+      try {
+        const user = supabase.auth.user();
+
+        const { error } = await supabase
+          .from("profiles")
+          .update({date: number}) // go to this column
+          .eq('id', session.user.id)   // find the specific user
+
+
+        if (error) throw error;
+
+      } catch (error) {
+        alert(error.error_description || error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+
+
+    return (
+      <>
+        <Header session={session} />
       <div className="App">
         <ChatwootWidget />
       </div>
@@ -20,7 +72,7 @@ const VideoPage = ({ session }) => {
             <iframe
               width="1000"
               height="500"
-              src={`https://www.youtube.com/embed/G_wxQe2Litw`}
+              src={link}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
@@ -36,7 +88,7 @@ const VideoPage = ({ session }) => {
         <br></br>
         <br></br>
         <Footer />
-      </div>
+      </>
     );
 }
 export default VideoPage;
