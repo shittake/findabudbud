f0e2aec76a4c912b43b3a7710db76fd47d97c6e6
Felicia Mah <e0564015@u.nus.edu>
2022-07-21 19:02:36 +0800

    add first test file

---
+++ cypress/e2e/events_spec.cy.js
@@ -0,0 +1,18 @@
+// describe("events", () => {
+//   //add events
+//   beforeEach(() => {
+//     cy.visit("/");
+//   });
+
+//   it("add events", () => {
+//     cy.findByRole("button", {
+//       name: /category ​/i,
+//     }).type("Games");
+//   });
+// });
+
+// describe("My First Test", () => {
+//   it("Does not do much!", () => {
+//     expect(true).to.equal(true);
+//   });
+// });
