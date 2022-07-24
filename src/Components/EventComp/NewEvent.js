259ade5b292d73c488b093bd9d1c41e0ee294fee
Felicia Mah <e0564015@u.nus.edu>
2022-07-24 15:15:57 +0800

    edit styles for eventspage bar and add functionality to toggle view my events button

--- src/Components/EventComp/NewEvent.js
+++ src/Components/EventComp/NewEvent.js
@@ -11,6 +11,7 @@ import Checkbox from "@mui/material/Checkbox";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { BlueButton, GreenButton, RedButton } from "../Buttons/ColouredButtons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //add: created by user
 
@@ -65,62 +66,82 @@ function NewEvent(props) {
     props.onSaveFilterData(filterData);
   };
 
+  const [viewMyEvents, setViewMyEvents] = useState(false);
+
   const onClickViewMyEventsHandler = (event) => {
     console.log(props.session.user.id);
     setCategory([]);
     props.onViewMyEvents(props.session.user.id);
+    setViewMyEvents(!viewMyEvents);
   };
   return (
     <>
       <EventForm onSaveEnteredEvent={saveEnteredEventHandler}></EventForm>
-      <div className="new-event">
-        <div>Filter by: </div>
-        <FormControl sx={{ m: 1, width: 300 }}>
-          <InputLabel id="demo-multiple-checkbox-label">Category</InputLabel>
-          <Select
-            labelId="demo-multiple-checkbox-label"
-            id="demo-multiple-checkbox"
-            multiple
-            value={category}
-            onChange={handleChange}
-            input={<OutlinedInput label="Tag" />}
-            renderValue={(selected) => selected.join(", ")}
-            MenuProps={MenuProps}
-          >
-            {categorylist.map((cat) => (
-              <MenuItem key={cat} value={cat}>
-                <Checkbox checked={category.indexOf(cat) > -1} />
-                <ListItemText primary={cat} />
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <TextField
-          type="number"
-          id="event id"
-          label="Event Id"
-          onChange={eventIdChangeHandler}
-          value={eventId}
-        />
-        <GreenButton
-          type="submit"
-          text="Filter Now!"
-          variant="contained"
-          onClick={onClickFilterHandler}
-        ></GreenButton>
-        <RedButton
-          type="submit"
-          text="Reset Filters!"
-          variant="contained"
-          onClick={onClickResetHandler}
-        ></RedButton>
-        <BlueButton
-          type="submit"
-          text="View My Events!"
-          variant="contained"
-          onClick={onClickViewMyEventsHandler}
-        ></BlueButton>
-      </div>
+      {!viewMyEvents && (
+        <div className="new-event">
+          <div>Filter by: </div>
+          <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Category</InputLabel>
+            <Select
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              multiple
+              value={category}
+              onChange={handleChange}
+              input={<OutlinedInput label="Tag" />}
+              renderValue={(selected) => selected.join(", ")}
+              MenuProps={MenuProps}
+            >
+              {categorylist.map((cat) => (
+                <MenuItem key={cat} value={cat}>
+                  <Checkbox checked={category.indexOf(cat) > -1} />
+                  <ListItemText primary={cat} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <TextField
+            type="number"
+            id="event id"
+            label="Event Id"
+            onChange={eventIdChangeHandler}
+            value={eventId}
+          />
+          <GreenButton
+            type="submit"
+            text="Filter Now!"
+            variant="contained"
+            onClick={onClickFilterHandler}
+          ></GreenButton>
+          <RedButton
+            type="submit"
+            text="Reset Filters!"
+            variant="contained"
+            onClick={onClickResetHandler}
+          ></RedButton>
+          <BlueButton
+            type="submit"
+            text={viewMyEvents ? "View All Events!" : "View My Events"}
+            variant="contained"
+            onClick={onClickViewMyEventsHandler}
+          ></BlueButton>
+        </div>
+      )}
+      {viewMyEvents && (
+        <>
+          <div className="new-event-alternative">
+            <div>Here are the events you registered for!</div>
+            <div>
+              <BlueButton
+                type="submit"
+                text={viewMyEvents ? "View All Events!" : "View My Events"}
+                variant="contained"
+                onClick={onClickViewMyEventsHandler}
+              ></BlueButton>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
