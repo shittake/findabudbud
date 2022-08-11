763679d9e66ddc6627ac40be49f7ffa9aee3c301
Felicia Mah <e0564015@u.nus.edu>
2022-08-11 18:27:15 +0800

    correct link when user signs out

--- src/Footer.js
+++ src/Footer.js
@@ -3,41 +3,42 @@ import styled from "styled-components";
 import { Typography } from "@material-ui/core";
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
+import { Navigate, useNavigate } from "react-router-dom";
 
-class Footer extends Component {
-  render() {
-    const onSignOut = async () => {
-      const updateUsersOnlineFalse = async () => {
-        try {
-          console.log(this.props.session.user.id);
-          const { error } = await supabase
-            .from("profiles")
-            .update({ userOnline: 0 }) // go to this column
-            .eq("id", this.props.session.user.id);
-          console.log("finish");
-          console.log(error);
+const Footer = ({ session }) => {
+  const navigate = useNavigate();
+  const onSignOut = async () => {
+    const updateUsersOnlineFalse = async () => {
+      try {
+        console.log(session.user.id);
+        const { error } = await supabase
+          .from("profiles")
+          .update({ userOnline: 0 }) // go to this column
+          .eq("id", session.user.id);
+        console.log("finish");
+        console.log(error);
 
-          // if (error) throw error;
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      await updateUsersOnlineFalse();
-      supabase.auth.signOut();
-      // add logic
-      console.log(this.props.session);
+        // if (error) throw error;
+      } catch (error) {
+        console.log(error);
+      }
     };
-    return (
-      <>
-        {/* Sign out button */}
-        <div className="footer">
-          <button className="button block" onClick={onSignOut}>
-            Sign Out
-          </button>
-        </div>
-      </>
-    );
-  }
-}
+    await updateUsersOnlineFalse();
+    supabase.auth.signOut();
+    navigate("/");
+    // add logic
+    console.log(session);
+  };
+  return (
+    <>
+      {/* Sign out button */}
+      <div className="footer">
+        <button className="button block" onClick={onSignOut}>
+          Sign Out
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
